0c8e9dc786397d841a7c699912e1afb9840db791
Vitalii <seminenkovs@gmail.com>
2023-04-02 02:28:21 +0200

    ASCII ART

---
+++ BitmapToASCIIConverter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASCIIArt
+{
+    internal class BitmapToASCIIConverter
+    {
+        private readonly char[] _asciiTable = { '.', ',', ':', '+', '*', '?', '%', '$', '#', '@' };
+        private readonly char[] _asciiTableNegative = { '@', '#', '$', '%', '?', '*', '+', ':', ',', '.' };
+        private readonly Bitmap _bitmap;
+        public BitmapToASCIIConverter(Bitmap bitmap)
+        {
+            _bitmap = bitmap;
+        }
+
+        public char[][] Convert()
+        {
+            return Convert(_asciiTable);
+        }
+
+        public char[][] ConvertAsNegative()
+        {
+            return Convert(_asciiTableNegative);
+        }
+
+        /// <summary>
+        /// Convert black and white image of pixels int to image of ascii symbols
+        /// </summary>
+        private char[][] Convert(char[] asciiTable)
+        {
+            var result = new char[_bitmap.Height][];
+
+            for (int y = 0; y < _bitmap.Height; y++)
+            {
+                result[y] = new char[_bitmap.Width];
+                for (int x = 0; x < _bitmap.Width; x++)
+                {
+                    int mapIndex = (int)Map(_bitmap.GetPixel(x, y).R, 0, 255, 0, asciiTable.Length - 1);
+                    result[y][x] = asciiTable[mapIndex];
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Map Range of 256 elements to Range of 10 elements
+        /// </summary>
+        /// <param name="valueTop"></param>
+        /// <param name="start1"></param>
+        /// <param name="stop1"></param>
+        /// <param name="start2"></param>
+        /// <param name="stop2"></param>
+        /// <returns></returns>
+        private float Map(float valueTop, float start1, float stop1, float start2, float stop2)
+        {
+            return ((valueTop - start1) / (stop1 - start1)) * (stop2 - start2) + start2;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using ASCIIArt;
+using System.IO;
+
+class Program
+{
+    private const double WIDTH_OFFSET = 1.7;
+    private const int MAX_WIDTH = ;
+
+    [STAThread]
+    static void Main()
+    {
+        var openFileDialog = new OpenFileDialog
+        {
+            Filter = "Image Files|*.bmp;*.png;*.jpg;*.JPEG"
+        };
+
+        Console.WriteLine("Press enter to start... \n");
+
+        while(true)
+        {
+            Console.ReadLine();
+
+            if (openFileDialog.ShowDialog() != DialogResult.OK)
+            {
+                continue;
+            }
+
+            Console.Clear();
+
+            var bitmap = new Bitmap(openFileDialog.FileName);
+            bitmap = ResizeBitmap(bitmap);
+            bitmap.ToGrayscale();
+
+            var convert = new BitmapToASCIIConverter(bitmap);
+            var rows = convert.Convert();
+
+            foreach (var row in rows)
+            {
+                Console.WriteLine(row);
+            }
+
+            var rowsNegative = convert.ConvertAsNegative();
+            File.WriteAllLines("image.txt", rowsNegative.Select(r => new string(r)));
+
+            Console.SetCursorPosition(0, 0);
+        }
+    }
+
+    /// <summary>
+    /// Resize image to fit in the console
+    /// </summary>
+    /// <param name="bitmap"></param>
+    /// <returns>Bitmap</returns>
+    private static Bitmap ResizeBitmap(Bitmap bitmap)
+    {
+        var newHeight = bitmap.Height / WIDTH_OFFSET * MAX_WIDTH / bitmap.Width;
+        if (bitmap.Width > MAX_WIDTH || bitmap.Height > newHeight)
+            bitmap = new Bitmap(bitmap, new Size(MAX_WIDTH, (int)newHeight));
+        return bitmap;
+    }
+}
+
+
+
+
+
+
+
